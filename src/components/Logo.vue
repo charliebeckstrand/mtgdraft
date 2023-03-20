1ed489d3607139ce9abd9edd2dbb8969001b12fa
Charlie Beckstrand <charlie.beckstrand@icloud.com>
2023-03-19 19:17:05 -0600

    feat: made logo dynamic

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import Animals from '@/common/animals'
+
+const randomNumber = (Math.random() * (Animals.length - 1)).toFixed()
+
+const Animal = `/animals/${Animals[randomNumber]}.png`
+
+const favicon = document.getElementById('favicon')
+
+if (Animal) {
+	favicon.href = Animal
+}
+</script>
 
 <template>
-	<img src="/dragon.png" class="w-12" />
+	<img :src="Animal" class="w-12" />
 </template>
 
 <style scoped></style>
